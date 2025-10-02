1eb617241152f3d44709f33fbc9d13322b8902ae
Jessica <JessicaEmail>
2025-10-02 07:05:26 -0400

    added test code to run a model

---
+++ base_single_layer_model.py
@@ -0,0 +1,31 @@
+"""
+Purpose:
+    Give a basic neural network that we could use to solve this problem.
+    I am trying to keep it as simple as possible to run test code and such, but to still be technically viable.
+    
+Input layer:
+    Standard 14 nodes, ints in [0,48], representing the board state.
+
+Internal layers:
+    1 hidden layer with 16 nodes
+
+Output layer:
+    1 value in [0,5] representing the hole to choose.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class base_single_layer_model(nn.Module):
+    def __init__(self):
+        super(base_single_layer_model, self).__init__()
+        self.fc1 = nn.Linear(14, 16) 
+        self.fc2 = nn.Linear(16, 6)
+
+    def forward(self, x):
+        x = x.float()
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return torch.argmax(x, dim=1)
+    
---
+++ network_tools.py
@@ -0,0 +1,39 @@
+from base_single_layer_model import base_single_layer_model
+import torch
+import numpy as np
+
+np.random.seed(42)
+
+
+#This should probably be moved to the game or board infrastructure
+#Perhaps return a board object, that can convert to torch on its own?
+def random_board():
+    out = np.random.multinomial(48, [1/14.]*14, size=1)[0]
+    sum = out.sum()
+    assert sum == 48, f"Error: invalid randomly generated board with {sum=} stones"
+    #print(f"{out=}")
+    return out
+
+def run_model_on_random_inputs(model):
+    """
+    Purpose:
+        Test a model with random inputs just to make sure it runs.
+    """
+    #Generate a random instance of model
+    torch.manual_seed(42)
+    net = model()
+    print(f"{net=}")
+
+    #Generate 5 random board states
+    inputs = [random_board() for _ in range(5)]
+    #print(f"{inputs=}")
+    inputs = torch.tensor(np.stack(inputs))
+
+    #Run the model
+    outputs = net(inputs)
+    #print(f"{outputs=}")
+    
+    for i,o in zip(inputs, outputs):
+        print(f"{i} mapped to {o}")
+
+run_model_on_random_inputs(base_single_layer_model)
